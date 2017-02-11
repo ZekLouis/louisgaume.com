335cbdef6efdbf96727a9eae8a8d9852a3a900c2
ZekLouis <zeklouis@gmail.com>
2017-02-11 13:11:39 +0100

    Gulp / SASS

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+
+gulp.task('sass',function(){
+  return gulp.src('css/style.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('css'))
+})
+
+gulp.task('default',function(){
+  gulp.watch('css/style.scss', ['sass'])
+})
